34a06090268cd4f369b76960e3a26f11a8da24c4
Almir2021 <bosnjicalmir@gmail.com>
2021-09-21 16:23:50 +0200

    Vjezba 2 ispravljena

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -4,14 +4,14 @@ function myFunction(myParameter) {
 
     if (myParameter == true) {
 
-        console.log("The parameter is true!")
+        return "The parameter is true!"
     }
 
     else {
-        console.log("The parameter is false!")
+        return "The parameter is false!"
     }
 }
-myFunction();
+console.log(myFunction());
 
 
 // Only change code above this line
