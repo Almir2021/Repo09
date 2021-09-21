e33a9fbef6d75315b8bb511f6c7e4a6dd080ad68
Almir2021 <bosnjicalmir@gmail.com>
2021-09-22 01:33:13 +0200

    Vjezba 16

---
+++ ex15/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+function myDoWhile() {
+    var myNumbers = 0;
+    while (myNumbers < 10) {
+        console.log(myNumbers);
+        myNumbers++;
+
+
+    }
+
+    return myNumbers;
+
+} myDoWhile();
+
+
+
+// Only change code above this line
+module.exports = myDoWhile;
