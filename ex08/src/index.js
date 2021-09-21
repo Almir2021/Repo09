c52c92976303a6a7d92e937f4e8a9045e453a59a
Almir2021 <bosnjicalmir@gmail.com>
2021-09-21 18:44:29 +0200

    Vjezba 8 or

---
+++ ex08/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+
+function logicalOrOperator(num) {
+    if (num < 20 || num > 30) {
+        return "Out"
+    }
+
+    else {
+        return "In"
+    }
+}
+
+console.log(logicalOrOperator(23));
+
+// Only change code above this line
+module.exports = logicalOrOperator;
