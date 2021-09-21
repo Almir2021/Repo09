a7864a6f0aa4465f4c8a8432f958ce5766a66499
Almir2021 <bosnjicalmir@gmail.com>
2021-09-21 17:12:14 +0200

    Vjezba != equal

---
+++ ex05/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+
+
+function compareStrictValues(m) {
+    if (m != 55) {
+        return "Not equal"
+
+    } else {
+        return "Equal"
+
+    }
+}
+console.log(compareStrictValues("doe"));
+
+// Only change code above this line
+module.exports = compareStrictValues;
