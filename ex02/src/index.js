b4ffa9d4a614b05fff30a3d5e64c531b4cf938dc
Almir2021 <bosnjicalmir@gmail.com>
2021-09-21 15:47:46 +0200

    Vjezba true or false

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line
+
+function myFunction(myParameter) {
+
+    if (myParameter == true) {
+
+        console.log("The parameter is true!")
+    }
+
+    else {
+        console.log("The parameter is false!")
+    }
+}
+myFunction();
+
+
+// Only change code above this line
+module.exports = myFunction;
