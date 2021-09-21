85ae8f4868e4f566cae671259fcedcba880c69a8
Almir2021 <bosnjicalmir@gmail.com>
2021-09-21 14:33:26 +0200

    Vjezba 1 boolean

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function myTrueBoolean() {
+
+    console.log(Boolean(1 == 1))
+
+}
+myTrueBoolean()
+
+
+// Only change code above this line
+
+module.exports = myTrueBoolean;
