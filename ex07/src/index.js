9e5e04527764c9efd30ea57e26818297ee446e5a
Almir2021 <bosnjicalmir@gmail.com>
2021-09-21 18:16:53 +0200

    Vjezba 7 radi

---
+++ ex07/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+
+function logicalAndOperator(num) {
+    if (num <= 80 && num >= 40) {
+        return "Yes"
+    }
+
+    else {
+        return "No"
+    }
+}
+
+console.log(logicalAndOperator(45));
+
+// Only change code above this line
+module.exports = logicalAndOperator;
