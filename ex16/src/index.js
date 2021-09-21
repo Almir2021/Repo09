de498bfc470c04ef1769b855c1bc58d0a4688eb5
Almir2021 <bosnjicalmir@gmail.com>
2021-09-22 01:54:30 +0200

    Vjezba 16

---
+++ ex16/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+function checkSing(num) {
+    return (num === 0) ? "zero"
+        : (num > 0) ? "positive"
+            : "negative";
+
+}
+console.log(checkSing(0));
+
+
+// Only change code above this line
+module.exports = checkSing;
